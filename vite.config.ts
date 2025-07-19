175355147cd6a4f3e3554ebc2adf8dbc624e297c
toughbox <nowonstation@gmail.com>
2025-07-19 23:35:18 +0900

    Gateway 작업 중

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 8090,
+    strictPort: true,
+    host: true,
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
